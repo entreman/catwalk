726b440d4d9c4f71fd6470984c9d7b9ae978c18b
entreman <entreman@protonmail.com>
2026-07-18 19:04:16 +0200

    Add class SlideShowController

---
+++ src/slideshowcontroller.py
@@ -0,0 +1,26 @@
+
+
+class SlideshowController:
+    def __init__(self, delay):
+        self.delay = delay
+        self.paused = False
+
+    def increase_delay(self):
+        self.delay += 1
+        print(f"Increase delay to: {self.delay}")
+
+    def decrease_delay(self):
+        self.delay = max(0, self.delay - 1)
+        print(f"Decrease delay to: {self.delay}")
+
+    def toggle_pause(self):
+        self.paused = not self.paused
+        print(f"Toggle pause: Currently paused: {self.paused}")
+
+        if not self.paused:
+            pass
+            #self.next_image()
+
+if __name__ == "__main__":
+    delay = 3
+    state = SlideshowController(delay)
