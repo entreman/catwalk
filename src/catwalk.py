3801f65dbf999761820ccf1ee65dbe3897d38b9e
entreman <entreman@protonmail.com>
2026-07-19 12:44:10 +0200

    Add navigation controls: Next, Previous
    Also:
    - Restructered everything into a class: CatwalkApp
    - Add presentation_queue

--- src/catwalk.py
+++ src/catwalk.py
@@ -9,115 +9,25 @@ import requests
 import tkinter as tk
 import sys
 
-
 from PIL import Image, ImageTk
 from pathlib import Path
 
-
 from slideshowcontroller import SlideshowController
-        
-URL = "https://cataas.com/cat"
+from catwalkapp import CatwalkApp
+   
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "delay",
+        nargs="?",
+        type=int,
+        default=3,
+        help="seconds between downloads"
+    )
+    args = parser.parse_args()
 
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "delay",
-    nargs="?",
-    type=int,
-    default=3,
-    help="seconds between downloads"
-)
-args = parser.parse_args()
-controller = SlideshowController(delay=args.delay)
-
-
-def resource_path(relative_path):
-    if hasattr(sys, "_MEIPASS"):
-        return Path(sys._MEIPASS) / relative_path
-    return Path(__file__).resolve().parent.parent / relative_path
-
-
-def downloader(stop_event, image_queue):
-    while not stop_event.is_set():
-        try:
-            response = requests.get(URL, timeout=10)
-            response.raise_for_status()
-
-            image = Image.open(io.BytesIO(response.content))
-
-            # wait until there is room
-            image_queue.put(image, timeout=1)
-            print(f"Queue Size: {image_queue.qsize()}")
-
-        except queue.Full:
-            pass
-
-        except Exception as e:
-            print("Download error:", e)
-
-
-
-def update_image():
-    if not controller.paused:
-        try:
-            image = controller.get_current_image()
-
-            image.thumbnail((screen_width, screen_height))
-
-            photo = ImageTk.PhotoImage(image)
-
-            label.configure(image=photo)
-            label.image = photo
-
-        except queue.Empty:
-            pass
-
-    root.after(int(controller.delay * 1000), update_image)
-
-
-def shutdown(event=None):
-    stop_event.set()
-    root.destroy()
-
-
-
-
-#image_queue = queue.Queue(maxsize=10)
-stop_event = threading.Event()
-
-
-root = tk.Tk()
-root.title("Catwalk")
-root.attributes("-fullscreen", True)
-root.configure(bg="black")
-root.config(cursor="none")
-
-icon_path = "assets/catwalk.png"
-icon = tk.PhotoImage(file=resource_path(icon_path))
-root.iconphoto(True, icon)
-
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-label = tk.Label(root, bg="black")
-label.pack(expand=True)
-
-
-threading.Thread(
-    target=downloader,
-    args=(stop_event, controller.image_queue),
-    daemon=True
-).start()
-
-
-root.bind("<Escape>", shutdown)
-root.bind("<space>", lambda e: controller.toggle_pause())
-root.bind("<Up>", lambda e: controller.decrease_delay())
-root.bind("<Down>", lambda e: controller.increase_delay())
-root.bind("<Left>", lambda e: controller.previous_image())
-root.bind("<Right>", lambda e: controller.next_image())
-
-update_image()
-
-root.mainloop()
+    app = CatwalkApp(delay=args.delay)
+    app.run()
--- src/slideshowcontroller.py
+++ src/slideshowcontroller.py
@@ -8,9 +8,10 @@ class SlideshowController:
         self.delay = delay
         self.paused = False
         self.maxlength = 20
-        self.current_index = 0
-        self.image_queue = queue.Queue(maxsize=self.maxlength)
-        self.history = deque(maxlen=self.maxlength)
+        self.current_index = -1
+        self.downloaded_queue = queue.Queue(maxsize=self.maxlength)
+        self.history = deque(maxlen=100)
+        self.presentation_queue = queue.Queue(maxsize=self.maxlength)
 
 
     def increase_delay(self):
@@ -33,25 +34,45 @@ class SlideshowController:
             #self.next_image()
         
     def next_image(self):
+        print("next_image")
         self.current_index +=1
-        print(f"current_index: {self.current_index}")
+        if len(self.history) < self.current_index + 1:
+            if not self.fetch_next_image_from_downloaded_queue():
+                self.current_index -= 1      
+        image = self.history[self.current_index]
+        print(f"Current Image: {self.current_index+1}/{len(self.history)} [Downloaded: {self.downloaded_queue.qsize()}]")
+        self.present_image(image)
 
     def previous_image(self):
+        print("previous_image")
         if self.current_index > 0:
             self.current_index -= 1
+        print(f"Current Image: {self.current_index+1}/{len(self.history)} [Downloaded: {self.downloaded_queue.qsize()}]")
+        image = self.history[self.current_index]
+        self.present_image(image)
+
+
+    def fetch_next_image_from_downloaded_queue(self):
+        try:
+            image = self.downloaded_queue.get_nowait()
+            self.history.append(image)
+            return True
+
+        except queue.Empty:
+            return False
+
+        print(f"History Size: {len(self.history)}")
+
 
     def get_current_image(self):
-        print("get_current_image")
-        if len(self.history) < self.current_index + 1:
-            self.fetch_next_image_from_queue()           
-        image = self.history[self.current_index]
-        self.next_image()
-        return image
+        image = self.presentation_queue.get_nowait()
+        print(f"presentation_queue: {self.presentation_queue.qsize()}")
+        return image        
     
-    def fetch_next_image_from_queue(self):
-        image = self.image_queue.get()
-        self.history.append(image)
-        print(f"History Size: {len(self.history)}")
+    def present_image(self, image):
+        # wait until there is room
+        self.presentation_queue.put_nowait(image)
+        print(f"presentation_queue: {self.presentation_queue.qsize()}")
 
 
 
