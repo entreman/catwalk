4c996de9153d389fecc45c65800f8fe7b2890166
entreman <entreman@protonmail.com>
2026-07-20 00:53:53 +0200

    Add feature: GUI for slideshow controls  (#10)
    * Add control bar + show cursor
    
    * cleaned away some log prints
    
    * Add Auto-hide for UI
    
    * center the control bar
    
    * Add control bar bottom margin and set background to black
    
    * Add dependency: customtkinter
    
    * Switched to CustomTkinter for round buttons
    
    * Add Previous Button
    
    * Add Next Button
    
    * Add Pause/Play Button
    
    * Add control bar UI icons and add mentions
    
    * Add Plus and Minus icons
    
    * Add Pause Icons
    
    * Link icons to control bar buttons
    
    * Changed control bar layout
    
    * Add Download Button
    
    * Add Decrease/Increase Delay Buttons
    
    * Add delay_label
    + small change to controller.increase_delay()
    
    * Polish control bar corners

--- src/catwalkapp.py
+++ src/catwalkapp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import customtkinter as ctk
 import requests
 import io
 import queue
@@ -14,6 +15,7 @@ class CatwalkApp:
         self.URL = "https://cataas.com/cat"
         self.delay = delay
         self.paused = False
+        self.miliseconds_before_ui_hides = int(3 * 1000) # ms
         self.controller = SlideshowController(self.delay)
 
 
@@ -29,11 +31,13 @@ class CatwalkApp:
 
         
     def _init_gui(self):
-        self.root = tk.Tk()
+        ctk.set_appearance_mode("dark")
+
+        self.root = ctk.CTk()
         self.root.title("Catwalk")
         self.root.attributes("-fullscreen", True)
-        self.root.configure(bg="black")
-        self.root.config(cursor="none")
+        self.root.configure(fg_color="black")
+        #self.root.config(cursor="none")
 
         icon_path = "assets/catwalk.png"
         icon = tk.PhotoImage(file=resource_path(icon_path))
@@ -45,15 +49,220 @@ class CatwalkApp:
         self.label = tk.Label(self.root, bg="black")
         self.label.pack(expand=True)
 
+
+        self._init_control_bar()
+
+    def _init_control_bar_icons(self):
+        self.play_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/play_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/play_interface_icon_dark.png")),
+            size=(24, 24)
+        )
+
+        self.pause_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/pause_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/pause_interface_icon_dark.png")),
+            size=(24, 24)
+        )
+
+        self.previous_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/previous_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/previous_interface_icon_dark.png")),
+            size=(24, 24)
+        )
+
+        self.next_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/next_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/next_interface_icon_dark.png")),
+            size=(24, 24)
+        )
+
+        self.download_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/download_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/download_interface_icon_dark.png")),
+            size=(18, 18)
+        )
+
+        self.done_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/check_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/check_interface_icon_dark.png")),
+            size=(18, 18)
+        )
+
+        self.delay_down_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/minus_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/minus_interface_icon_dark.png")),
+            size=(16, 16)
+        )
+
+        self.delay_up_icon = ctk.CTkImage(
+            light_image=Image.open(resource_path("assets/control_bar/plus_interface_icon_light.png")),
+            dark_image=Image.open(resource_path("assets/control_bar/plus_interface_icon_dark.png")),
+            size=(16, 16)
+        )
+
+    def _init_control_bar(self):
+        self._init_control_bar_icons()
+
+        self.control_bar = ctk.CTkFrame(
+            self.root,
+            fg_color="#222222",
+            corner_radius= 2
+        )
+
+        button_config = {
+            "width": 50,
+            "height": 40,
+            "corner_radius": 22.5,
+            "font": ("DejaVu Sans", 22),
+            "fg_color": "#333333",
+            "hover_color": "#444444"
+        }
+
+        # --- left section ---
+        self.left_controls = ctk.CTkFrame(
+            self.control_bar,
+            fg_color="transparent"
+        )
+
+        self.download_button = ctk.CTkButton(
+            self.left_controls,
+            text="",
+            image=self.download_icon,
+            **button_config,
+            command=self.on_d
+        )
+
+        self.download_button.pack(side="left", padx=10, pady=10)
+
+
+        # --- center section ---
+        self.center_controls = ctk.CTkFrame(
+            self.control_bar,
+            fg_color="transparent"
+        )
+
+        self.previous_button = ctk.CTkButton(
+            self.center_controls,
+            text="",
+            image=self.previous_icon,
+            command=self.show_previous,
+            **button_config
+        )
+
+        self.play_pause_button = ctk.CTkButton(
+            self.center_controls,
+            text="",
+            image=self.pause_icon,
+            command=self.toggle_pause,
+            **button_config
+        )
+
+        self.next_button = ctk.CTkButton(
+            self.center_controls,
+            text="",
+            image=self.next_icon,
+            command=self.show_next,
+            **button_config
+        )
+
+        self.previous_button.pack(side="left", padx=10, pady=10)
+        self.play_pause_button.pack(side="left", padx=10, pady=10)
+        self.next_button.pack(side="left", padx=10, pady=10)
+
+
+        # --- right section ---
+        self.right_controls = ctk.CTkFrame(
+            self.control_bar,
+            fg_color="transparent"
+        )
+
+        self.delay_down_button = ctk.CTkButton(
+            self.right_controls,
+            text="",
+            **button_config,
+            image=self.delay_down_icon,
+            command=self.decrease_delay
+        )
+
+        self.delay_label = ctk.CTkLabel(
+            self.right_controls,
+            text=f"{self.controller.delay}s",
+            width=40
+        )
+
+        self.delay_up_button = ctk.CTkButton(
+            self.right_controls,
+            text="",
+            **button_config,
+            image=self.delay_up_icon,
+            command=self.increase_delay
+        )
+
+        self.delay_down_button.pack(side="left", padx=5, pady=10)
+
+        self.delay_label.pack(side="left", padx=5, pady=10)
+
+        self.delay_up_button.pack(side="left", padx=5, pady=10)
+        # --- right section end ---
+
+
+        # Pack the three groups
+        self.left_controls.pack(
+            side="left",
+            padx=20
+        )
+
+        self.center_controls.pack(
+            side="left",
+            padx=20
+        )
+
+        self.right_controls.pack(
+            side="left",
+            padx=20
+        )
+
+
+        # Bind Enter, for all control bar elements
+        for widget in [
+            self.control_bar,
+            
+            self.left_controls,
+            self.download_button,
+            
+            self.center_controls,
+            self.previous_button,
+            self.play_pause_button,
+            self.next_button,
+            
+            self.right_controls,
+            self.delay_down_button,
+            self.delay_label,
+            self.delay_up_button
+        ]:
+            widget.bind("<Enter>", self.mouse_enter_bar)
+            widget.bind("<Leave>", self.mouse_leave_bar)
+
+        # Control Bar Bindings:
+        self.control_bar.bind("<Enter>", self.mouse_enter_bar)
+        self.control_bar.bind("<Leave>", self.mouse_leave_bar)
+
+        self.mouse_over_bar = False
+
+        self.show_ui()
+
+
     def _init_keybinds(self):
         self.root.bind("<Escape>", self.shutdown)
         self.root.bind("<space>", self.on_space)
-        self.root.bind("<Up>", lambda e: self.controller.decrease_delay())
-        self.root.bind("<Down>", lambda e: self.controller.increase_delay())
+        self.root.bind("<Up>", lambda e: self.decrease_delay())
+        self.root.bind("<Down>", lambda e: self.increase_delay())
         self.root.bind("<Left>", self.on_left)
         self.root.bind("<Right>", self.on_right)
         self.root.bind("<Key-d>", self.on_d)
         self.root.bind("<Key-D>", self.on_d)
+        self.root.bind("<Motion>", self.mouse_moved)
 
     def on_left(self, event=None):
         self.show_previous()
@@ -67,7 +276,54 @@ class CatwalkApp:
     def on_d(self, event=None):
         print("D")
         self.controller.download_current_image()
+        self.download_button.configure(
+            image=self.done_icon
+        )
         
+    def mouse_moved(self, event=None):
+        self.show_ui()
+
+    def mouse_enter_bar(self, event=None):
+        print("enter_bar")
+        self.mouse_over_bar = True
+
+    def mouse_leave_bar(self, event=None):
+        print("leave bar")
+        self.mouse_over_bar = False
+        self.reset_ui_hide_timer()
+
+
+
+    def hide_ui(self):
+        if self.mouse_over_bar:
+            return
+
+        print("hide UI")
+        self.control_bar.place_forget()
+        self.root.config(cursor="none")
+
+    def show_ui(self):
+        print("show UI")
+        self.control_bar.place(
+            relx=0.5,
+            rely=0.99,
+            anchor="s"
+        )
+        self.root.config(cursor="")
+        self.reset_ui_hide_timer()
+
+
+    def start_ui_hide_timer(self):
+        self.ui_timer_id = self.root.after(self.miliseconds_before_ui_hides, self.hide_ui)
+
+    def stop_ui_hide_timer(self):
+        if hasattr(self, "ui_timer_id"):
+            self.root.after_cancel(self.ui_timer_id)
+            self.ui_timer_id = None
+
+    def reset_ui_hide_timer(self):
+        self.stop_ui_hide_timer()
+        self.start_ui_hide_timer()
 
 
     def downloader(self, stop_event, image_queue, url):
@@ -117,10 +373,17 @@ class CatwalkApp:
         print(f"Toggle pause: Currently paused: {self.paused}")
 
         if self.paused:
+            self.play_pause_button.configure(
+                image=self.play_icon
+            )
             self.stop_timer()
+            
         else:
+            self.play_pause_button.configure(
+                image=self.pause_icon
+            )
             self.reset_timer()  # Start timer again, after being paused. 
-
+            
 
     def update_image(self):
         try:
@@ -144,10 +407,38 @@ class CatwalkApp:
         self.controller.previous_image()
         self.reset_timer()
 
+        self.download_button.configure(
+            image=self.download_icon
+        )
+
     def show_next(self):
         self.controller.next_image()
         self.reset_timer()
+
+        self.download_button.configure(
+            image=self.download_icon
+        )
     
+
+    def increase_delay(self):
+        self.controller.increase_delay()
+        self.update_delay_label()
+
+    def decrease_delay(self):
+        self.controller.decrease_delay()
+        self.update_delay_label()
+        self.reset_timer()
+
+    def update_delay_label(self):
+        if self.controller.delay.is_integer():
+            text = f"{int(self.controller.delay)}s"
+        else:
+            text = f"{self.controller.delay:.1f}s"
+        
+        self.delay_label.configure(
+            text=text
+        )
+
 
     def shutdown(self, event=None):
         self.stop_event.set()
